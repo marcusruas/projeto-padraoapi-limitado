207ba6ef8f4d9a69bbb93854fabaf9b9f13fac85
marcusruas <marcus.ruas@hotmail.com>
2019-06-08 13:44:52 -0300

    Adicionado camada de domínio

---
+++ Dominio/Usuario/IUsuarioDominio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Usuario {
+    public interface IUsuarioDominio {
+        Guid IdUsuario { get; set; }
+        string Nome { get; set; }
+        string Usuario { get; set; }
+        string Senha { get; set; }
+        string Grupo { get; set; }
+        DateTime DataCriacao { get; set; }
+        bool Ativo { get; set; }
+        string Cpf { get; set;  }
+    }
+}
---
+++ Dominio/Usuario/UsuarioDominio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Usuario {
+    public class UsuarioDominio : IUsuarioDominio {
+        public UsuarioDominio() {  
+        }
+
+        public UsuarioDominio(Guid idUsuario, string nome, string usuario, string senha, string grupo, DateTime dataCriacao, bool ativo, string cpf) {
+            IdUsuario = idUsuario;
+            Nome = nome;
+            Usuario = usuario;
+            Senha = senha;
+            Grupo = grupo;
+            DataCriacao = dataCriacao;
+            Ativo = ativo;
+            Cpf = cpf;
+        }
+
+        [Description("IDUSUARIO")]
+        public Guid IdUsuario { get; set; }
+        [Description("NOME")]
+        public string Nome { get; set; }
+        [Description("USUARIO")]
+        public string Usuario { get; set; }
+        [Description("SENHA")]
+        public string Senha { get; set; }
+        [Description("GRUPO")]
+        public string Grupo { get; set; }
+        [Description("DATACRIACAO")]
+        public DateTime DataCriacao { get; set; }
+        [Description("ATIVO")]
+        public bool Ativo { get; set; }
+        [Description("CPF")]
+        public string Cpf { get; set; }
+    }
+}
