d7dc5092f1f44161b2754dfbdfe46d0b9ff3bd9a
marcusruas <marcus.ruas@hotmail.com>
2019-06-08 13:56:43 -0300

    Criado camada de repositório

---
+++ Repositorio/Implementacao/StatusAplicacaoRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using Repositorio.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repositorio.Implementacao {
+    public class StatusAplicacaoRepositorio : IStatusAplicacaoRepositorio {
+        private IConexaoBanco Conexao;
+        public StatusAplicacaoRepositorio(IConexaoBanco conexao) {
+            Conexao = conexao;
+        }
+
+        public string StatusBanco() {
+            string resultado = string.Empty;
+            try {
+                resultado = Conexao.Select<string>("selectStatusBanco", "SHAREDB").First().ToString();
+            } catch(Exception e) {
+                return e.Message;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Repositorio/Interface/IStatusAplicacaoRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repositorio.Interface {
+    public interface IStatusAplicacaoRepositorio {
+        string StatusBanco();
+    }
+}
