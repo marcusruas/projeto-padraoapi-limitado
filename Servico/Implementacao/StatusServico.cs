0113195e47258203a90181249ec54b0418e9385f
marcusruas <marcus.ruas@hotmail.com>
2019-06-08 14:43:24 -0300

    Adicionado camada de serviço

---
+++ Servico/Implementacao/StatusServico.cs
@@ -0,0 +1,27 @@
+﻿using Repositorio.Interface;
+using Servico.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servico.Implementacao {
+    public class StatusServico : IStatusServico {
+        private readonly IStatusAplicacaoRepositorio _repositorio;
+
+        public StatusServico(IStatusAplicacaoRepositorio repositorio) {
+            _repositorio = repositorio;
+        }
+
+        public string VerificarStatusBanco() {
+            string status = string.Empty;
+            try {
+                status = _repositorio.StatusBanco();
+                return status;
+            } catch (Exception e) {
+                return e.Message;
+            }
+        }
+
+    }
+}
