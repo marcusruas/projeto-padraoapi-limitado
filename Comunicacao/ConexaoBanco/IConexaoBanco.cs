11a19fc301025c2f3cf4eb84b8ab1f3e8077fc7f
marcusruas <marcus.ruas@hotmail.com>
2019-06-23 22:11:32 -0300

    Adicionado metodos na interface

--- Comunicacao/ConexaoBanco/IConexaoBanco.cs
+++ Comunicacao/ConexaoBanco/IConexaoBanco.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace Comunicacao.ConexaoBanco {
     public interface IConexaoBanco {
+        void IniciarConexao(string nomeBanco);
+        void FecharConexao();
         void IniciarTransacao(string nomeBanco);
         void Commit();
         void Rollback();
         IEnumerable<T> Select<T>(string nomeArquivo, string nomeBanco);
+        T SelectUmaLinha<T>(string nomeArquivo, string nomeBanco);
+        int Executar<T>(string nomeArquivo, string nomeBanco, T modelo);
+
     }
 }
